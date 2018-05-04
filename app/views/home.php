51cfb030128699a99c39bbf76b3f32f6b005585f
Taufan Prasetyo Basri <taufanprasetyobasri@gmail.com>
2018-05-04 09:07:09 +0700

    Basic templating

---
+++ app/views/home.php
@@ -0,0 +1,2 @@
+<h1>Welcome in To-Do App</h1>
+<p class="lead">Simple task manager app.</p>
---
+++ app/views/layouts/main.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+    <head>
+        <meta charset="utf-8">
+        <title>To-Do Task Manager</title>
+		<link rel="stylesheet" href="<?= base_url() ?>assets/css/bootstrap.min.css">
+		<link rel="stylesheet" href="<?= base_url() ?>assets/css/custom.css">
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    </head>
+    <body>
+		<div class="navbar navbar-default">
+	      <div class="container">
+	        <div class="navbar-header">
+	          <a href="#" class="navbar-brand">To-Do</a>
+	        </div>
+	        <div class="navbar-collapse collapse" id="navbar-main">
+	          <ul class="nav navbar-nav">
+	            <li>
+	              <a href="<?= base_url() ?>">Home</a>
+	            </li>
+	          </ul>
+
+	          <ul class="nav navbar-nav navbar-right">
+	            <!-- <li><a href="http://builtwithbootstrap.com/" target="_blank">Built With Bootstrap</a></li> -->
+	          </ul>
+
+	        </div>
+	      </div>
+	    </div>
+
+		<div class="container">
+			<div class="page-header">
+		        <div class="row">
+					<div class="col-lg-3 col-md-3 col-sm-4">
+						<div class="list-group table-of-contents">
+						<a class="list-group-item" href="#">Navbar</a>
+						</div>
+					</div>
+					<div class="col-lg-9 col-md-9 col-sm-8">
+						<!-- Main Content -->
+						<?php $this->load->view($main_content); ?>
+					</div>
+		        </div>
+			</div>
+
+			<footer>
+		        <div class="row">
+		          <div class="col-lg-12 text-center">
+		            <p>&copy; Copyright 2018</p>
+		          </div>
+		        </div>
+			</footer>
+		</div>
+    </body>
+</html>
